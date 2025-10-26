ca6de24bf6bb5ff299b61c20456e2905cc75977a
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2025-10-26 17:57:45 -0600

    Delete file

--- df.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("Check").getOrCreate()
-
-df = spark.read.parquet("./data/rag_parquet/chunks/")
-df.show(5, truncate=False)
